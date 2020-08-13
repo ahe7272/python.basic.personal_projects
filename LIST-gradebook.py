84afaeac8876a1e49300b9261134bafed368ea17
Heeone Lee <ahe7272@naver.com>
2020-08-13 15:06:38 +0900

    List practice

--- LIST-gradebook.py
+++ LIST-gradebook.py
@@ -2,6 +2,7 @@ last_semester_gradebook = [("Math", 90), ("Korean", 75), ("Art", 87), ("Science"
 
 subjects = ["Physics", "History", "English", "Geology"]
 grades = [98, 97, 85, 88]
+subjects.append("Computer science")
 grades.append(100)
 gradebook = list(zip(subjects, grades))
 gradebook.append(("Visual arts", 93))
